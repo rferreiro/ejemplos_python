f4c9911b10f194a430f484caf88441e9ec33de5e
Roberto <roberto.ferreiro@gmail.com>
2024-03-05 11:38:18 +0100

    numeros

---
+++ numeros.py
@@ -0,0 +1,18 @@
+# Pruebas de números
+print(2)
+print(2.5)
+
+#Operadores
+print(2 + 3)
+print(8 - 4)
+
+# 4 x 4 = 16 x 4 = 64
+print(4 ** 3)
+
+#El paréntesis se resolverá primero
+print((2 + 3) * 4)
+
+numero = 20
+print(numero)
+numero += 1 
+print(type(numero))
