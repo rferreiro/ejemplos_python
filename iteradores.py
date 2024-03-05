68e3bc618d4b4c4b5c74aeb55bf78428f4d385df
Roberto <roberto.ferreiro@gmail.com>
2024-03-05 12:47:09 +0100

    iteradores

---
+++ iteradores.py
@@ -0,0 +1,9 @@
+lenguajes = ['Python', 'Kotlin', 'Java', 'JavaScript']
+
+# Iterador
+for lenguaje in lenguajes:
+    print(f'Estoy aprendiendo {lenguaje}')
+
+# For que escriba números
+for numero in range(0,20):
+    print(numero)
